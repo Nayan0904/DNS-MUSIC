760f37bc7dca5e6534f4e1933f013dd390ec8c46
Nayan0904 <99829000+Nayan0904@users.noreply.github.com>
2024-08-02 17:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/mr_nayan_49",
+    "Contact Owner",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
